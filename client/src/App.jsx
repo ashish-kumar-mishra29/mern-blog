baf5ac3bd84318c17fac84541476e7535aaa039b
ashish-kumar-mishra29 <ashishkumar.mishra110702@marwadiuniversity.ac.in>
2024-05-19 12:19:41 +0530

    Pages Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 
 export default function App() {
-  return <div className="text-3xl text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
